97589529919620b21346c95ce76f9a7a68f77043
gaurav <gauravgola96@gmail.com>
2020-11-02 17:02:49 +0530

    Images app added : Create view

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['localhost','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    
+    'images.apps.ImagesConfig',
     'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
